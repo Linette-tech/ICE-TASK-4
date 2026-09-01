66d0f707d6d13bc97452291916023ba36bf65c47
Linette-tech <st10457818@rcconnect.edu.za>
2026-09-01 09:02:29 +0200

    Completed Question 2 Staff Hiring

--- StaffHiring/src/staffhiring/UseStaff.java
+++ StaffHiring/src/staffhiring/UseStaff.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package staffhiring;
 
-/**
- *
- * @author CASH
- */
+import java.util.Scanner;
+
 public class UseStaff {
-    
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Get staff number
+        System.out.print("Enter the current staff number: ");
+        int staffNumber = input.nextInt();
+        input.nextLine();
+
+        // Get staff location
+        System.out.print("Enter the staff hiring location: ");
+        String staffLocation = input.nextLine();
+
+        // Create StaffHiring object
+        StaffHiring staff = new StaffHiring(staffNumber, staffLocation);
+
+        // Display the report
+        staff.printStaffHiring();
+    }
 }
